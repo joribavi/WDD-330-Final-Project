ff10b8a23856f66d74922265152e4c8ca4fcc847
Jonathan Villegas <jvillegas86@gmail.com>
2026-02-02 12:04:54 -0300

    creating functions to render header and footer each one with its template

---
+++ cody/src/utils.mjs
@@ -0,0 +1,27 @@
+//
+export function renderWithTemplate(template, parentElement, data, callback) {
+    parentElement.innerHTML = template;
+    if(callback) callback(data);
+}
+
+export async function loadTemplate(path) {
+    const res = await fetch(path);
+    return await res.text();
+}
+
+export async function loadHeaderFooter(callback){
+    const headerTemplate = await loadTemplate("/partials/header.html");
+    const footerTemplate = await loadTemplate("/partials/footer.html");
+
+    const headerElement = document.querySelector(".my-Header");
+    const footerElement = document.querySelector(".my-Footer");
+
+    if(headerElement) {
+        renderWithTemplate(headerTemplate, headerElement, null, callback);
+       }
+     if(footerElement) {
+        renderWithTemplate(footerTemplate, footerElement, null, callback);
+       }
+   
+
+}
