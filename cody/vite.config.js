8e07927716bff8df9ff74549c06f61c98beb429e
Jonathan Villegas <jvillegas86@gmail.com>
2026-02-09 17:33:53 -0300

    working on possible issue with vite.config file

--- cody/vite.config.js
+++ cody/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'  
 
 export default defineConfig({
-  root: "src", //src will be the root directory for vite
+  root: "src/", //src will be the root directory for vite
   build: {
      outDir: '../dist' ,
      rullupOptions: {
