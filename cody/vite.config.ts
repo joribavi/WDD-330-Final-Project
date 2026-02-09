aedce29f7f0323e6cac633c6a3b55b759eb40604
Jonathan Villegas <jvillegas86@gmail.com>
2026-02-09 12:17:04 -0300

    changing file and folder structure

--- cody/vite.config.ts
+++ cody/vite.config.ts
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  root: "src", //src will be the root directory for vite
+  build: {
+     outDir: '../dist' 
+    
+  }
+       
+  ,
+
   plugins: [
     tailwindcss(),
-  ],
-})
+  ]
+
+});
