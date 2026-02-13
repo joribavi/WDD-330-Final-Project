0f508da1cf1ff665ec6737cb714d6c3c37e2b07c
Jonathan Villegas <jvillegas86@gmail.com>
2026-02-13 12:46:52 -0300

    starting work with API modules

---
+++ src/js/api.mjs
@@ -0,0 +1,30 @@
+import { Octokit } from "octokit"; //import Octokit in order to fetch data from Github API
+
+export async function gitApiFetch(seachTopic){
+
+    const octokit = new Octokit({}); //creating octokit Instance without Auth 
+
+
+  try {
+
+     const response =  await octokit.request("GET /search/repositories", {
+      q: `topic:${seachTopic} in:description`, //search after user input in the description
+      per_page: 10,    
+      headers: {
+        "x-github-api-version": "2022-11-28",
+      },
+    
+     });
+  
+    return console.log(response.data); // for testing purposes
+
+  } catch (error) {
+    if (error.status === 404) {
+    console.log(`something is wrong`);
+    } else {
+      console.error(`An error occurred: ${error?.response?.data?.message}`);
+   }
+  }
+
+
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import { gitApiFetch  } from "./api.mjs"; // for testing purposes
+
 
 await loadHeaderFooter();
+
+await gitApiFetch("loops");
