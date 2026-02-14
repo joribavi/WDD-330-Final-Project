fd5df4e6f01dbb1b3b80721a716a1ffd370d64fd
Jonathan Villegas <jvillegas86@gmail.com>
2026-02-13 21:15:25 -0300

    working on search module

---
+++ src/js/search.mjs
@@ -0,0 +1,13 @@
+import { gitApiFetch } from "./api.mjs";
+import { openLibraryFetch } from "./api.mjs";
+
+export async function searchInput() {
+const searchBar = document.getElementById("search");
+
+searchBar.addEventListener("input", (e)=> {
+    
+
+});
+
+} ; 
+
